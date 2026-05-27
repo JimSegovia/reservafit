888ca89a86f6866390dfcabdbf1f15103c327ba4
SEBASTIAN <sebastian.chicata@gmail.com>
2026-05-27 00:17:17 -0500

    feat(Cuentas): Se creó el flujo de los datos de la tabla Cuentas

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes.js';
+import cuentaRoutes from './cuenta.routes.js';
 import instructorRoutes from './instructor.routes.js';
 import claseRoutes from './clase.routes.js';
-import detalleClaseRoutes from './detalleClase.routes.js'; 
+import detalleClaseRoutes from './detalleClase.routes.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/cuentas', cuentaRoutes); 
 router.use('/instructores', instructorRoutes);
 router.use('/clases', claseRoutes);
-router.use('/agenda', detalleClaseRoutes); 
+router.use('/agenda', detalleClaseRoutes);
 
 export default router;
